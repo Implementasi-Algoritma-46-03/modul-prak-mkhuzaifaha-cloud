f3f18218d586f5c54dfa48781b5e08b49b244645
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-10-01 07:06:54 +0700

    write something here

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -5,12 +5,12 @@ public class TP01 {
        int b = 10000;
        int c = 30;
        int perkalian = c * b + a;
-       System.out.println(c * b + a);
+       System.out.println(perkalian);
 
        int d = 250000;
        int e = 25000;
        int f = 30;
        int perkalian2 = f * e + d;
-       System.out.println(f * e + d);  
+       System.out.println(perkalian2);  
     }
 }
--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -4,11 +4,10 @@ public class TP02 {
         // Declare and initializing variables
 
         //integral variable
-         String Hi = "Dira."; 
+         String nama = "Dira."; 
          String t = " rupiah";
          int b = 5;
          int c = 2000;
-         int perkalian = c * b ;
-         System.out.println("Hi, " + Hi + " Total belanja adalah " + (b * c) + t);
+         System.out.println("Hi, " + nama + " Total belanja adalah " + (b * c) + t);
     }
 }
