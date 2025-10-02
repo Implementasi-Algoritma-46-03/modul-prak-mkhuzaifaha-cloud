7faae9b8be4758d2bf1a5ff94f4f456503707b84
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-10-02 17:06:35 +0700

    fjava

--- Modul03/Jurnal01.java
+++ Modul03/Jurnal01.java
@@ -1,6 +1,18 @@
+
+import java.util.Scanner;
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner sc = new Scanner(System.in);
+    System.err.println("Masukkan Nominal Awal : ");
+    int nominalawal = sc.nextInt();
+    int modulan = nominalawal % 1000;
+    int bagi = nominalawal / 1000;
+    System.out.println(bagi);
+    System.out.println(modulan);
+    
+
+    sc.close();
     }
 }
--- Modul03/Jurnal03.java
+++ Modul03/Jurnal03.java
@@ -1,6 +1,18 @@
+
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner sc = new Scanner(System.in);
+        double NA = sc.nextDouble();
+        double P=sc.nextDouble();
+        double A= sc.nextDouble();
+        double hasil = NA *0.25 + P * 0.35 + A * 0.4;
+
+        System.out.printf(" %.2f\n", hasil);
+        boolean isLulus = hasil >= 75;
+        isLulus = hasil <= 75;
+        System.out.println("Lulus MK: " + isLulus);
     }
 }
