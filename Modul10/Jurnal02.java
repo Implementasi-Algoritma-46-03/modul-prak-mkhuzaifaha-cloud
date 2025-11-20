9646ac62ade589b204804ed3492c6d81c21ebc86
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-11-20 16:58:13 +0700

    Jurnal02 modul10

--- Modul10/Jurnal02.java
+++ Modul10/Jurnal02.java
@@ -4,8 +4,12 @@ public class Jurnal02 {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        for (i = 0; i <= x; 1++) {
+        for (int i = 1; i <= x; i++) {
 
+            for(int j = 1; j <= x; j++) {
+                System.out.printf("%-4d" , i*j);
+            }
+            System.out.println();
         }
 
     }
