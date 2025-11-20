4ceb60ad25188bf55fed654f6a459e2ec351134f
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-11-20 16:50:44 +0700

    j1 10

--- Modul10/Jurnal02.java
+++ Modul10/Jurnal02.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (i = 0; i <= x; 1++) {
+
+        }
+
     }
 }
