e6d675ddf761f772f57127ff46e0219929cce922
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-11-20 16:43:36 +0700

    Jurnal1 modul10

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -2,17 +2,20 @@ public class Jurnal01 {
 
     public static void main(final String[] args) {
       
-       for (int pola = 1; pola <= 6; pola++){
+       for (int pola = 0; pola <= 6; pola++){
         if (pola % 2 ==0){
             System.out.print(" ");
         } 
             for (int spasi = 1; spasi <= 5; spasi++){
-            System.out.print(pola + " ");
+            System.out.print(pola);
+
+            if (spasi < 5 || pola % 2 != 0){
+                System.out.print(" ");
             }
-            
-         System.out.println();
        }
+       System.out.println();
     }
+}
 }
 
     
