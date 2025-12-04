aa6a5de8321fd772c95b2991c1347a7818d07ac3
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-12-04 16:08:27 +0700

    jurnal01 modul 12 fix

--- Modul12/Jurnal01.java
+++ Modul12/Jurnal01.java
@@ -11,10 +11,16 @@ public class Jurnal01 {
               }
          }
             for (int i = N - 1; i >= 0; i--) {
-                for (int j = 0; j < N; j++) {
-                    System.out.print(matriks[i][j] + " ");
-                }
-                System.out.println();
-            }
+    for (int j = 0; j < N; j++) {
+        if (j == N - 1) {
+            // elemen terakhir di baris -> tanpa spasi
+            System.out.print(matriks[i][j]);
+        } else {
+            // elemen selain terakhir -> dengan spasi
+            System.out.print(matriks[i][j] + " ");
+        }
+    }
+    System.out.println();
+        }
     }
 }
