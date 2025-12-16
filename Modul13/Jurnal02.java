66d91586817f3c37e544a22fde218ccb3c5b37f1
mkhuzaifaha-cloud <mkhuzaifaha@gmail.com>
2025-12-16 15:48:04 +0700

    jurnal 02 03 m13

--- Modul13/Jurnal02.java
+++ Modul13/Jurnal02.java
@@ -1,6 +1,27 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] barang = sc.nextLine().split(" ");
+         for (int i = 1; i < n; i++) {
+            String urutan = barang[i];
+            int j = i -1;
+
+            while (j >= 0 && barang[j].compareTo(urutan) > 0) {
+                barang [j + 1] = barang[j];
+                j--;
+            }
+            barang[j + 1] = urutan;
+         }
+         for (int i = 0; i < n; i++) {
+                System.out.print(barang[i]);
+                if (i < n -1) System.out.print(" ");
+
+        }
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-    }
-}
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] barang = sc.nextLine().split(" ");
+        String urutan = sc.nextLine();
+
+        for (int i = 1; i < n; i++) {
+            String temp = barang[i];
+            int j = i - 1;
+            while (j >= 0 && barang[j].compareTo(temp) > 0){
+                barang[j + 1] = barang[j];
+                j--;
+            }
+            barang[j + 1] = temp;
+        }
+        int kiri = 0, kanan = n -1;
+        int Index = -1;
+
+        while (kiri <= kanan) {
+            int tengah = (kiri + kanan) / 2;
+            int cmp = barang[tengah].compareTo(urutan);
+            
+            if (cmp == 0) {
+                Index = tengah;
+                break; 
+            } else if (cmp < 0) {
+                kiri = tengah + 1;
+            } else {
+                kanan = tengah - 1;
+            }
+        }
+                if (Index != -1) {
+                    System.out.println("Ditemukan di indeks ke-" + Index);
+                } else {
+                    System.out.println("Data tidak ditemukan");
+                }
+            }
+        }
+    
+
